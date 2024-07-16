db3251911e9c7c33ba91029047e1d8b53782f363
janvi-chhirang <153886981+janvi-chhirang@users.noreply.github.com>
2024-07-16 17:15:58 +0530

    Add files via upload

---
+++ intro-section(incomplete)/intro-section-with-dropdown-navigation-main/script.js
@@ -0,0 +1,4 @@
+const changeImage=document.querySelector('.right-part img')
+if(outerWidth<=375){
+changeImage.src="C:\Users\DELL\Desktop\web-dev\projects\intro-section\intro-section-with-dropdown-navigation-main\images\image-hero-mobile.png"
+}
